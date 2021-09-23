e88f7411bca34b72c116be41a2393b3adbba860b
Rahmat Nurrizki Solehudin <mametjunior212@gmail.com>
2021-09-23 15:45:18 +0700

    activity DSR/DBR

--- app/src/main/java/com/application/bris/ikurma_nos_konsumer/page_data_nasabah/Activity_DSR_DBR_Nasabah.java
+++ app/src/main/java/com/application/bris/ikurma_nos_konsumer/page_data_nasabah/Activity_DSR_DBR_Nasabah.java
@@ -10,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.application.bris.ikurma_nos_konsumer.databinding.ActivityDsrDbrNasabahBinding ;
 import com.application.bris.ikurma_nos_konsumer.util.NumberTextWatcherCanNolForThousand;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Activity_DSR_DBR_Nasabah extends AppCompatActivity {
     private ActivityDsrDbrNasabahBinding binding;
 
@@ -19,12 +22,14 @@ public class Activity_DSR_DBR_Nasabah extends AppCompatActivity {
         binding = ActivityDsrDbrNasabahBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        binding.etSisapenghasilan.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etSisapenghasilan));
-        binding.etAngsuranPinjamanEksisting.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etAngsuranPinjamanEksisting));
-        binding.etMaxAngsuran.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etMaxAngsuran));
-        binding.etDsrDbr.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etDsrDbr));
-        binding.etSisaDsrDbr.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etSisaDsrDbr));
-        binding.etSisapenghasilan.addTextChangedListener(new TextWatcher() {
+        numberText();
+        onchangeText();
+
+
+    }
+
+    private void onchangeText(){
+        binding.etAngsuranPinjamanEksisting.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -32,7 +37,17 @@ public class Activity_DSR_DBR_Nasabah extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                BigDecimal verangsuran = new BigDecimal(NumberTextWatcherCanNolForThousand.trimCommaOfString( binding.etAngsuranPinjamanEksisting.getText().toString()));
+                BigDecimal vertotverpendapatan = new BigDecimal(NumberTextWatcherCanNolForThousand.trimCommaOfString("10000"));
+                BigDecimal verparamProduk= new BigDecimal(NumberTextWatcherCanNolForThousand.trimCommaOfString("10000"));
+                BigDecimal sisaPendapatan  = (vertotverpendapatan.subtract(verangsuran));
+                BigDecimal dsrNasabah = (verangsuran.divide(vertotverpendapatan, 2, RoundingMode.HALF_EVEN).multiply(new BigDecimal(1)));
+                BigDecimal sisaDsr = (verparamProduk.subtract(sisaPendapatan));
+                BigDecimal max_angsuran = (vertotverpendapatan.multiply(sisaDsr));
+                binding.etSisapenghasilan.setText(String.valueOf(sisaPendapatan));
+                binding.etDsrDbr.setText(String.valueOf(dsrNasabah));
+                binding.etSisaDsrDbr.setText(String.valueOf(sisaDsr));
+                binding.etMaxAngsuran.setText(String.valueOf(max_angsuran));
             }
 
             @Override
@@ -40,6 +55,13 @@ public class Activity_DSR_DBR_Nasabah extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void numberText(){
+        binding.etSisapenghasilan.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etSisapenghasilan));
+        binding.etAngsuranPinjamanEksisting.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etAngsuranPinjamanEksisting));
+        binding.etMaxAngsuran.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etMaxAngsuran));
+        binding.etDsrDbr.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etDsrDbr));
+        binding.etSisaDsrDbr.addTextChangedListener(new NumberTextWatcherCanNolForThousand(binding.etSisaDsrDbr));
     }
 }
