6bad0161f6c1afa6019605f9a6ec72bf67cbbc31
Rahmat Nurrizki Solehudin <mametjunior212@gmail.com>
2021-12-08 16:50:14 +0700

    request

---
+++ app/src/main/java/com/application/bris/ikurma_nos_konsumer/api/model/request/prapen/ReqDocumentUmum.java
@@ -0,0 +1,52 @@
+package com.application.bris.ikurma_nos_konsumer.api.model.request.prapen;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ReqDocumentUmum {
+
+    @SerializedName("NamaDokumen")
+    @Expose
+    private String namaDokumen;
+    @SerializedName("KeteranganDokumen")
+    @Expose
+    private String keteranganDokumen;
+    @SerializedName("File_Name")
+    @Expose
+    private String filename;
+    @SerializedName("Img")
+    @Expose
+    private String img;
+
+    public String getNamaDokumen() {
+        return namaDokumen;
+    }
+
+    public void setNamaDokumen(String namaDokumen) {
+        this.namaDokumen = namaDokumen;
+    }
+
+    public String getKeteranganDokumen() {
+        return keteranganDokumen;
+    }
+
+    public void setKeteranganDokumen(String keteranganDokumen) {
+        this.keteranganDokumen = keteranganDokumen;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
